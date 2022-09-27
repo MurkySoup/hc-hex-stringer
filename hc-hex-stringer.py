0d1eaaa26425796b6568c51c45b30995b8e6e032
Rick Pelletier <galiagante@gmail.com>
2022-09-27 10:24:43 -0500

    Initial commit

---
+++ hc-hex-stringer.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""
+Hashcat Hex Password Format Converter, Version 0.4-Beta (Do Not Distribute)
+By Rick Pelletier (galiagante@gmail.com), 25 September 2022
+Last update: 27 September 2022
+
+Example usage:
+
+# ./hc-hex-stringer.py --string 'marquee:' --encode
+$HEX[6d6172717565653a]
+
+# ./hc-hex-stringer.py --string '$HEX[262333393a313539373533]' --decode
+&#39:159753
+"""
+
+
+import sys
+import argparse
+
+
+def is_hexstring(string:str):
+  try:
+    for s in string:
+      int(s, 16)
+
+    return True
+  except:
+    return False
+
+
+def hc_hex_to_str(string:str):
+  if string.startswith('$HEX[') and string.endswith(']') and is_hexstring(string[5:-1]):
+    return(f'{bytes.fromhex(string[5:-1]).decode("utf-8")}')
+  else:
+    return False
+
+
+def str_to_hc_hex(string:str):
+  return(f'$HEX[{string.encode("utf-8").hex()}]')
+
+
+if __name__ == '__main__':
+  exit_code = 0
+  parser = argparse.ArgumentParser()
+  option_group = parser.add_argument('-s', '--string', help='String to en-/de-code', type=str, required=True)
+  command_group = parser.add_mutually_exclusive_group(required=True)
+  command_group.add_argument('-e', '--encode', help='Operation to encode a string', action='store_true')
+  command_group.add_argument('-d', '--decode', help='Operation to decode a string', action='store_true')
+
+  args = parser.parse_args()
+
+  if args.encode:
+    encoded = str_to_hc_hex(args.string)
+    print(f'{encoded}')
+
+  if args.decode:
+    decoded = hc_hex_to_str(args.string)
+
+    if decoded is not False:
+      print(f'{hc_hex_to_str(args.string)}')
+    else:
+      print('Error decoding string')
+      exit_code = 1
+
+  sys.exit(exit_code)
+else:
+  sys.exit(1)
+
+# end of script
