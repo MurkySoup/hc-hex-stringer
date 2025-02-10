e83e4b50dd4fd652568884c569872bd04b3293fc
Rick pelletier <rpelletier@gannett.com>
2025-02-10 08:55:59 -0600

    Added decoding error bypass feature

--- hc-hex-stringer.py
+++ hc-hex-stringer.py
@@ -3,13 +3,13 @@
 
 
 """
-Hashcat Hex Password Format Converter, Version 0.8.1-Beta (Do Not Distribute)
+Hashcat Hex Password Format Converter, Version 0.9.1-Beta (Do Not Distribute)
 By Rick Pelletier (galiagante@gmail.com), 25 September 2022
-Last update: 02 January 2025
+Last update: 10 January 2025
 
 Help:
 
-usage: hc-hex-stringer.py [-h] (-s STRING | -f FILE) (-e | -d)
+usage: hc-hex-stringer.py [-h] [-i] (-s STRING | -f FILE) (-e | -d)
 
 options:
   -h, --help                    show this help message and exit
@@ -17,7 +17,7 @@ options:
   -f FILE, --file FILE          file to en-/de-code
   -e, --encode                  encode to hc hex format
   -d, --decode                  decode from hc hex format
-
+  -i, --ignore                  ignore decoding errors
 
 Example usage:
 
@@ -28,14 +28,18 @@ $HEX[6d6172717565653a]
 &#39:159753
 
 
+Successful en/decoding will return the converted string and an exit code of '0'.
+
 In the event of an encoding or decoding error (for example, chardet could not
 identify the correct character set to perform a conversion), nothing will be
-displayed, but an exit code of '1' will be returned to the O/S. Successful
-en/decoding will return the converted string and an exit code of '0'.
+displayed, but an exit code of '1' will be returned to the O/S. In the event
+that errors are bypassed via '--ignore' option, the return value will always
+be '0'.
 
-An option to process entire files and assumes one string per line. Lines that
-cannot be properly encoded or decoded are skipped. The focus is on task
-completion, rather than halting an every error.
+An option to process entire files has been included and assumes one string per
+line. Lines that cannot be properly encoded or decoded are skipped (unless
+'--ignore' is used). The focus is on task completion, rather than halting on
+and/or reporting very error.
 """
 
 
@@ -58,13 +62,15 @@ def is_hexstring(string:str):
         return False
 
 
-def hc_hex_to_str(hex_string:str):
+def hc_hex_to_str(hex_string:str, ignore_errors:bool = False):
+    errors_option = 'ignore' if ignore_errors else 'strict'
+
     if hex_string.startswith('$HEX[') and hex_string.endswith(']') and is_hexstring(hex_string[5:-1]):
         try:
             encoding_detection = chardet.detect(bytes.fromhex(hex_string[5:-1]))
 
-            return bytes.fromhex(hex_string[5:-1]).decode(encoding_detection['encoding'])
-        except Exception as e:
+            return bytes.fromhex(hex_string[5:-1]).decode(encoding_detection['encoding'], errors=errors_option)
+        except:
             return False
     else:
         return False
@@ -74,7 +80,7 @@ def str_to_hc_hex(string:str):
     try:
         return (f'$HEX[{string.encode("utf-8").hex()}]')
 
-    except Exception as e:
+    except:
         return False
 
 
@@ -124,6 +130,8 @@ if __name__ == '__main__':
     command_group_two.add_argument('-e', '--encode', help='encode to hc hex format',   action='store_true')
     command_group_two.add_argument('-d', '--decode', help='decode from hc hex format', action='store_true')
 
+    parser.add_argument('-i', '--ignore', help='ignore decoding errors', action='store_true', required=False)
+
     args = parser.parse_args()
 
     if args.file:
@@ -134,7 +142,7 @@ if __name__ == '__main__':
                 if process_file(args.file, 'encode') is False:
                     exit_code = 1
             if args.decode:
-                if process_file(args.file, 'decode') is False:
+                if process_file(args.file, 'decode', args.ignore) is False:
                     exit_code = 1
 
 
@@ -146,7 +154,7 @@ if __name__ == '__main__':
                 print(encoded)
 
         if args.decode:
-            if (decoded := hc_hex_to_str(args.string)) is False:
+            if (decoded := hc_hex_to_str(args.string), args.ignore) is False:
                 exit_code = 1
             else:
                 print(decoded)
